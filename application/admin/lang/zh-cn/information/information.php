20782a372f40d739e7e46b61ca75bda40cc7ed36
dmsansan <562404326@qq.com>
2018-11-06 12:23:03 +0800

    添加咨询语言项

---
+++ application/admin/lang/zh-cn/information/information.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'Id'             => 'ID',
+    'Title'          => '标题',
+    'Img_url'        => '图片链接',
+    'Link_url'       => '链接地址',
+    'Status'         => '显示/隐藏',
+    'Remark'         => '备注',
+    'Add_time'       => '添加时间',
+    'Upload to local'=> '上传到本地',
+    'Yes'            => '显示',
+    'No'             => '隐藏',
+    'No'             => '隐藏',
+    'Preview'        => '预览',
+];
