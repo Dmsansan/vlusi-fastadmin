8398d0b9ac1e259d25b3c1bb2c7e377f77dc96c3
wangerpangci <18037235202@163.com>
2018-11-10 14:54:28 +0800

    zan

--- application/api/controller/Found.php
+++ application/api/controller/Found.php
@@ -16,6 +16,7 @@ class Found extends Api
     protected $noNeedLogin = ['*'];
     protected $noNeedRight = ['*'];
     protected $pagesize= 10;
+    protected $userid=47;
 
     /**
      * 发现分类
@@ -114,6 +115,46 @@ class Found extends Api
     }
 
 
+
+    /**
+     * 文章点赞接口
+     *
+     * @ApiTitle    (对某文章进行点赞)
+     * @ApiMethod   (POST)
+     * @ApiRoute    (/api/found/article_zan)
+     * @ApiParams   (name="article_id", type="integer", required=true, description="文章id")
+     * @ApiHeaders  (name=token, type=string, required=true, description="请求的Token")
+     * @ApiReturnParams   (name="code", type="integer", required=true, sample="0")
+     * @ApiReturnParams   (name="msg", type="string", required=true, sample="返回成功")
+     * @ApiReturnParams   (name="data", type="object", sample="{'user_id':'int','user_name':'string','profile':{'email':'string','age':'integer'}}", description="扩展数据返回")
+     * @ApiReturn   ({
+    'code':'1',
+    'mesg':'返回成功'
+     * })
+     */
+    public function article_zan()
+    {
+        $userid=$this->userid;
+        $article_id  =  (int)$this->request->request("article_id");
+
+        $res=db('article_zan')->where(['user_id'=>$userid,'article_id'=>$article_id])->find();
+        if($res){$this->success("点赞成功");}
+
+        $insert['article_id']=$article_id;
+        $insert['user_id']=$userid;
+        $insert['createtime']=time();
+        $res=db('article_zan')->insert($insert);
+        if($res){
+            //同步新增到article_comment表 赞+1
+            db('article')->where('id', $article_id)->setInc('zan');
+
+            $this->success("点赞成功");
+        }else{
+            $this->error('点赞失败');
+        }
+    }
+
+
     /**
      * 获取文章详情及评论接口
      * @ApiMethod   (POST)
@@ -131,7 +172,7 @@ class Found extends Api
      */
     public function detail()
     {
-        $userid=47;
+        $userid=$this->userid;
 
         $page   =  (int)$this->request->post("page");
         $page = $page?$page-1:0;
@@ -149,6 +190,11 @@ class Found extends Api
             //TODO 获取用户信息 判断用户是否点赞
             $collection=db('article_collection')->where(['user_id'=>$userid,'article_id'=>$article_id])->find();
             $data['is_collection']=$collection?1:0;
+
+            $zan=db('article_zan')->where(['user_id'=>$userid,'article_id'=>$article_id])->find();
+            $data['is_zan']=$zan?1:0;
+
+
 
         }
 
@@ -188,7 +234,7 @@ class Found extends Api
      */
     public function comment()
     {
-        $userid=47;
+        $userid=$this->userid;
         $article_id  =  (int)$this->request->request("article_id");
         $content=$this->request->request("content");
 
@@ -224,8 +270,11 @@ class Found extends Api
      */
     public function comment_zan()
     {
-        $userid=47;
+        $userid=$this->userid;
         $comment_id  =  (int)$this->request->request("comment_id");
+        $res=db('article_comment_zan')->where(['user_id'=>$userid,'comment_id'=>$comment_id])->find();
+        if($res){$this->success("点赞成功");}
+
 
         $insert['comment_id']=$comment_id;
         $insert['user_id']=$userid;
@@ -235,9 +284,9 @@ class Found extends Api
             //同步新增到article_comment表 赞+1
             db('article_comment')->where('id', $comment_id)->setInc('zan');
 
-            $this->success("评论成功");
+            $this->success("点赞成功");
         }else{
-            $this->error('评论失败');
+            $this->error('点赞失败');
         }
     }
 
@@ -258,7 +307,7 @@ class Found extends Api
      */
     public function collection()
     {
-        $userid=47;
+        $userid=$this->userid;
 
         $article_id= (int)$this->request->request("article_id");
         $is_have=db('article_collection')->where(['article_id'=>$article_id,'user_id'=>$userid])->find();
@@ -275,13 +324,64 @@ class Found extends Api
         $this->success("收藏成功",[]);
     }
 
+    /**
+     * 获取某条评论详情
+     * @ApiMethod   (POST)
+     * @ApiRoute    (/api/found/comment_detail)
+     * @ApiParams   (name="comment_id", type="integer", required=true, description="该评论的id")
+     * @ApiHeaders  (name=token, type=string, required=true, description="请求的Token")
+     * @ApiReturnParams   (name="code", type="integer", required=true, sample="0")
+     * @ApiReturnParams   (name="msg", type="string", required=true, sample="返回成功")
+     * @ApiReturnParams   (name="data", type="object", sample="{'user_id':'int','user_name':'string','profile':{'email':'string','age':'integer'}}", description="扩展数据返回")
+     * @ApiReturn   ({
+    'code':'1',
+    'mesg':'返回成功'
+     * })
+     */
+
     public function comment_detail()
     {
-        
+        $comment_id= (int)$this->request->request("comment_id");
+        //评论内容
+//        $detail['detail']=db('article_comment')->alias('a')->join('user','user.id=a.user_id')
+//                ->field('user.nickname,avatar,a.*')
+//                ->where(['id'=>$comment_id])
+//                ->find();
+//        if($detail['detail']){
+//            $detail['detail']['createtime']=date('Y-m-d H:i:s',$detail['detail']['createtime']);
+//        }else{
+//            $this->success("获取成功",[]);
+//        }
+
+
+        $children=db('article_comment')->alias('a')->join('article_comment b','b.pid=a.id')
+//                ->join('user','user.id=a.user_id')
+                ->field('*')
+                ->select();
+
+        dump($children);die;
+
+
+
     }
 
 
 
+
+    protected function commentTree($id)
+    {
+        static $subs = array(); //子孙数组
+        $children_data=db('article_comment')->alias('a')
+                    ->where(['pid'=>$id])
+                    ->join('user u','u.id=a.user_id')
+                    ->field('user.nickname,avatar,a.*')
+                    ->select();
+
+        if($children_data){
+
+        }
+        return $subs;
+    }
 
 
 }
