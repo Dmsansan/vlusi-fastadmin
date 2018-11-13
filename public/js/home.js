44f69869b1e2f6c7a1ebffe66761a94fc7736b7a
wangerpangci <18037235202@163.com>
2018-11-13 17:04:03 +0800

    token

--- public/js/home.js
+++ public/js/home.js
@@ -1,6 +1,7 @@
 let set = new Set(JSON.parse(localStorage.getItem('history-home')));
 //设置token
-localStorage.setItem('token','a460f6f0b010dccb4560afeaaadfd5d161db044d');
+// localStorage.setItem('token','a460f6f0b010dccb4560afeaaadfd5d161db044d');
+localStorage.setItem('token',<?php echo $token;?>);
 let app = new Vue({
     el: '#app',
     data: {
