2430397f1892ddd134b9a4feb4ac5e3c56a06419
wangerpangci <18037235202@163.com>
2018-11-13 13:17:42 +0800

    callback

--- addons/third/controller/Index.php
+++ addons/third/controller/Index.php
@@ -84,6 +84,7 @@ class Index extends Controller
         // 成功后返回会员中心
         $url = url('index/user/index');
 
+        dump($platform);
         // 授权成功后的回调
         $result = $this->app->{$platform}->getUserInfo();
         dump($result);die;
