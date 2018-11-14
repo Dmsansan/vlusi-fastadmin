89e63ee10ef3277ded2ad9edd442df8a053ef5b6
wangerpangci <18037235202@163.com>
2018-11-14 08:33:17 +0800

    codin master

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -16,7 +16,7 @@ class User extends Frontend
 {
 
 //    protected $layout = 'default';
-    protected $noNeedLogin = ['login', 'register', 'third','set_data'];
+    protected $noNeedLogin = ['login', 'register', 'third'];
     protected $noNeedRight = '*';
 
 //    protected $userid;
