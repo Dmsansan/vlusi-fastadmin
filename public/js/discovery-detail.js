ee21c513bd8b6cc13a449f45a437d4c35d36d003
wangerpangci <18037235202@163.com>
2018-11-17 13:44:05 +0800

    debug

--- public/js/discovery-detail.js
+++ public/js/discovery-detail.js
@@ -3,50 +3,50 @@ $(function () {
             el: '#app',
             data: {
                 //第几页
-                pageNumber:1,
+                pageNumber: 1,
                 //页面总页数
-                pageCount:null,
+                pageCount: null,
                 //加载更多
-                loadMore:false,
+                loadMore: false,
                 //详情ID
-                passID:null,
+                passID: null,
                 //评论列表
-                reviewList:[],
-               /* isLikeArt:false,*/
-                likeNum:20,
+                reviewList: [],
+                /* isLikeArt:false,*/
+                likeNum: 20,
                 isPlay: false,
                 videoTotalTime: '00:00',
                 currentTime: '00:00',
                 videoPlayer: '',
                 //评论输入的内容
-                commentsContent:'',
+                commentsContent: '',
                 //发送按钮禁用
-                isDisabled:true,
+                isDisabled: true,
                 //是否有视频
-                isVideo:false,
+                isVideo: false,
                 //评论发表聚焦
                 isFocus: false,
-               /* //是否收藏
-                isCollect:false,*/
+                /* //是否收藏
+                 isCollect:false,*/
                 //上传图片地址数组
-                imgList:[],
+                imgList: [],
                 //是否展示卡片视图
-                isShowCard:false,
+                isShowCard: false,
                 //分享图片
-                sharePictures:'',
+                sharePictures: '',
                 //详情数据
-                detailsList:[],
-                formdata:new FormData(),
+                detailsList: [],
+                formdata: new FormData(),
                 //分享内容
-                imgUrl:'',
-                title:'',
-                desc:'',
-                shareUrl:'',
-                configWX:[],
+                imgUrl: '',
+                title: '',
+                desc: '',
+                shareUrl: '',
+                configWX: [],
             },
             mounted(){
-               /* //初始化数据
-                this.init();*/
+                /* //初始化数据
+                 this.init();*/
             },
             methods: {
                 touchStart (e) {
@@ -54,7 +54,7 @@ $(function () {
                 },
                 touchMove (e) {
                     if (e.targetTouches[0].pageY < this.startY) { // 上拉
-                        if(this.loadMore){
+                        if (this.loadMore) {
                             this.judgeScrollBarToTheEnd()
                         }
                     }
@@ -67,41 +67,41 @@ $(function () {
                     // 变量scrollHeight是滚动条的总高度
                     let scrollHeight = document.documentElement.clientHeight || document.body.scrollHeight
                     // 滚动条到底部的条件
-                    if (scrollTop + scrollHeight >= innerHeight-10000) {
+                    if (scrollTop + scrollHeight >= innerHeight - 10000) {
                         this.infiniteLoadDone()
                     }
                 },
                 infiniteLoadDone () {
                     let self = this;
                     //总页数
-                    if(self.pageCount >self.pageNumber){
-                        self.pageNumber +=1;
+                    if (self.pageCount > self.pageNumber) {
+                        self.pageNumber += 1;
                         $.post('/api/found/detail', {
-                            token:localStorage.getItem('token'),
+                            token: localStorage.getItem('token'),
                             article_id: self.passID,
                             page: self.pageNumber,
                         }, function (data) {
-                            data.data.comment.forEach(function (item,index) {
+                            data.data.comment.forEach(function (item, index) {
                                 self.reviewList.push(item)
                             });
                         });
-                    }else {
+                    } else {
                         return
                     }
                 },
                 //初始化数据
-                init:function () {
+                init: function () {
                     let self = this;
                     $.post(' /api/found/detail', {
                         article_id: self.passID,
-                        token:localStorage.getItem('token'),
+                        token: localStorage.getItem('token'),
                         page: self.pageNumber,
                     }, function (data) {
                         self.detailsList = data.data;
                         self.reviewList = data.data.comment;
                         self.pageCount = data.page.pageCount;
                         self.loadMore = true;
-                        self.isVideo = data.data.detail.videofile?true:false;
+                        self.isVideo = data.data.detail.videofile ? true : false;
                         //分享内容
                         self.imgUrl = data.data.detail.coverimage;
                         self.title = data.data.detail.name;
@@ -110,21 +110,41 @@ $(function () {
                             let shareData = {
                                 title: self.title, // 分享标题
                                 desc: self.desc, // 分享描述
-                                link:  self.shareUrl , // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                                link: self.shareUrl, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
                                 imgUrl: self.imgUrl, // 分享图标
-                                success:function(){
+                                success: function () {
                                     alert('111')
                                 },
                                 fail: function (res) {
                                     alert(JSON.stringify(res));
                                 }
                             };
-                            console.log(shareData);
+                                console.log(shareDate);
+                                console.log('begin');
 
-                            if(wx.onMenuShareAppMessage){
-                                wx.onMenuShareAppMessage(shareData);//1.0 分享到朋友
-                            }else {
-                                wx.updateAppMessageShareData(shareData);//1.4 分享到朋友
+                            if (wx.onMenuShareAppMessage) {
+                                wx.onMenuShareAppMessage({
+                                    title: self.title, // 分享标题
+                                    desc: self.desc, // 分享描述
+                                    link: self.shareUrl, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                                    imgUrl: self.imgUrl, // 分享图标
+                                    type: '', // 分享类型,music、video或link，不填默认为link
+                                    dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+                                    success: function () {
+                                        alert(1111);
+                                    }
+                                })
+                            } else {
+                                wx.updateTimelineShareData({
+                                    title: self.title, // 分享标题
+                                    desc: self.desc, // 分享描述
+                                    link: self.shareUrl, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                                    imgUrl: self.imgUrl, // 分享图标
+                                    success: function () {
+                                        // 设置成功
+                                        alert(141414);
+                                    }
+                                })
                             }
 
                         })
@@ -132,7 +152,7 @@ $(function () {
 
                 },
                 //上传图片
-                uploadImg:function () {
+                uploadImg: function () {
                     document.getElementById('upload-img').click();
                 },
                 //显示上传图片图标等
@@ -151,10 +171,10 @@ $(function () {
                 goBack: function () {
                     history.go(-1);
                 },
-                likeComment:function (flag,id) {
+                likeComment: function (flag, id) {
                     let self = this;
                     $.post(' /api/found/comment_zan', {
-                        token:localStorage.getItem('token'),
+                        token: localStorage.getItem('token'),
                         comment_id: id,
                     }, function (data) {
                         self.$nextTick(function () {
@@ -164,90 +184,90 @@ $(function () {
                 },
 
                 //文章点赞
-                likeArticle:function(id,flag) {
+                likeArticle: function (id, flag) {
                     let self = this;
                     $.post('/api/found/article_zan', {
                         article_id: id,
-                        token:localStorage.getItem('token')
+                        token: localStorage.getItem('token')
                     }, function (data) {
-                       /* self.$nextTick(function () {
-                            self.init();
-                        })*/
+                        /* self.$nextTick(function () {
+                         self.init();
+                         })*/
 
                     });
-                    if(flag) {
+                    if (flag) {
                         self.detailsList.is_zan = true;
-                        self.detailsList.detail.zan =++self.detailsList.detail.zan;
+                        self.detailsList.detail.zan = ++self.detailsList.detail.zan;
                         /*mui.toast('点赞成功！');*/
                     } else {
                         self.detailsList.is_zan = false;
                         self.detailsList.detail.zan = --self.detailsList.detail.zan;
-                       /* mui.toast('取消点赞！');*/
+                        /* mui.toast('取消点赞！');*/
                     }
                 },
                 //回复
-                replay:function () {
+                replay: function () {
                     $('.emoji-wysiwyg-editor').focus();
                 },
-                shareCourse:function() {
+                shareCourse: function () {
                     mui('#share-sheet').popover('toggle');
                 },
                 //收藏，取消收藏
-                collect:function (flag) {
+                collect: function (flag) {
                     let self = this;
                     $.post('/api/found/collection', {
                         article_id: self.passID,
-                        token:localStorage.getItem('token')
+                        token: localStorage.getItem('token')
                     }, function (data) {
                         self.$nextTick(function () {
                             //初始化数据
                             self.init();
                         })
                     });
-                   /* if(flag) {
-                        self.detailsList.is_collection = true;
-                        mui.toast('已收藏');
-                    }
-                    else {
-                        self.detailsList.is_collection = false;
-                        mui.toast('已取消收藏');
-                    }*/
+                    /* if(flag) {
+                     self.detailsList.is_collection = true;
+                     mui.toast('已收藏');
+                     }
+                     else {
+                     self.detailsList.is_collection = false;
+                     mui.toast('已取消收藏');
+                     }*/
                 },
                 //解决键盘遮挡
-                focusInput:function () {
-                  /*  let bfscrolltop = 0;//获取软键盘唤起前浏览器滚动部分的高度
-                    let interval;
-                    $('#form_article').focus(function() {
-                        //给个延迟
-                        bfscrolltop = document.body.scrollTop;//获取软键盘唤起前浏览器滚动部分的高度
-                        interval = setInterval(function() {
-                            document.body.scrollTop = document.body.scrollHeight}, 100
-                        );
-                        window.addEventListener('touchmove', function () {
-                            
-                        }, false);
+                focusInput: function () {
+                    /*  let bfscrolltop = 0;//获取软键盘唤起前浏览器滚动部分的高度
+                     let interval;
+                     $('#form_article').focus(function() {
+                     //给个延迟
+                     bfscrolltop = document.body.scrollTop;//获取软键盘唤起前浏览器滚动部分的高度
+                     interval = setInterval(function() {
+                     document.body.scrollTop = document.body.scrollHeight}, 100
+                     );
+                     window.addEventListener('touchmove', function () {
 
-                    }).blur(function(){
-                        clearInterval(interval);
-                    });*/
+                     }, false);
+
+                     }).blur(function(){
+                     clearInterval(interval);
+                     });*/
                 },
-                goCommentsDetail:function (id) {
+                goCommentsDetail: function (id) {
                     //查看评论详情
                     mui.openWindow({
-                        url:'/index/found/comments?id='+id
+                        url: '/index/found/comments?id=' + id
 
                     })
 
                 },
-                sendToFriend:function () {
+                sendToFriend: function () {
                     //发给好友
                     let self = this;
-                    self.shareUrl  = location.href.split('#')[0];
+                    self.shareUrl = location.href.split('#')[0];
                     $.post('/api/index/getShareSigna', {
                         url: encodeURIComponent(self.shareUrl),
-                        token:localStorage.getItem('token')
+                        token: localStorage.getItem('token')
                     }, function (data) {
-                        if(data.code == 1){
+                        if (data.code == 1) {
                             self.configWX = data.data;
                             self.$nextTick(function () {
                                 shareWeChat(self.configWX);
@@ -267,42 +287,43 @@ $(function () {
                             ] // 必填，需要使用的JS接口列表
                         });
                     }
+
                     /*wx.updateAppMessageShareData({
-                        title: self.title, // 分享标题
-                        desc: self.desc, // 分享描述
-                        link:  self.shareUrl , // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-                        imgUrl: self.imgUrl, // 分享图标
-                        success: function () {
-                            // 设置成功
-                        }
-                    });*/
+                     title: self.title, // 分享标题
+                     desc: self.desc, // 分享描述
+                     link:  self.shareUrl , // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                     imgUrl: self.imgUrl, // 分享图标
+                     success: function () {
+                     // 设置成功
+                     }
+                     });*/
 
                 },
-                blurFn:function () {
+                blurFn: function () {
                     let self = this;
                     self.isFocus = false;
                 },
-                generateCard:function () {
+                generateCard: function () {
                     let self = this;
                     mui('#share-sheet').popover('toggle');
-                    mui.showLoading("正在加载..","div");
+                    mui.showLoading("正在加载..", "div");
                     //生成卡片
                     self.isShowCard = true;
 
                     $.post('/api/share/getimage', {
                         article_id: self.passID,
-                        token:localStorage.getItem('token')
+                        token: localStorage.getItem('token')
                     }, function (data) {
                         self.sharePictures = data.data.url;
                         mui.hideLoading();
                     });
 
                 },
-                hidePreview:function () {
+                hidePreview: function () {
                     //关闭图片预览
                     this.isShowCard = false;
                 },
-                saveImg:function () {
+                saveImg: function () {
                     let self = this;
                     self.isDisabled = false;
                     $("#upload-img").change(function () {
@@ -320,21 +341,21 @@ $(function () {
 
                 },
                 //发布评论
-                sendInformation:function () {
+                sendInformation: function () {
                     let self = this;
                     self.isDisabled = true;
                     self.formdata.append('article_id', self.passID);
                     self.formdata.append('token', localStorage.getItem('token'));
                     self.formdata.append('content', self.commentsContent);
                     $.ajax({
-                        url:'/api/found/comment',
-                        type:'post',
-                        cache:false,
-                        data:self.formdata,
+                        url: '/api/found/comment',
+                        type: 'post',
+                        cache: false,
+                        data: self.formdata,
                         /*  dataType:'json',*/
-                        processData:false,
-                        contentType:false,
-                        success:function (data) {
+                        processData: false,
+                        contentType: false,
+                        success: function (data) {
                             self.$nextTick(function () {
                                 self.formdata = new FormData();
                                 self.commentsContent = '';
@@ -342,7 +363,7 @@ $(function () {
                                 self.init();
                             })
                         },
-                        error:function (data) {
+                        error: function (data) {
 
                         }
                     })
@@ -388,10 +409,10 @@ $(function () {
     document.querySelector('.emoji-wysiwyg-editor').addEventListener('blur', function () {
         let elem = $(this);
         setTimeout(function () {
-            if(app.commentsContent.length == 0 && app.imgList.length == 0) {
+            if (app.commentsContent.length == 0 && app.imgList.length == 0) {
                 app.showSmile();
             }
-        },500);
+        }, 500);
         document.querySelector('#uploadImg').addEventListener('click', function () {
             app.isUploadImage = true;
             app.uploadImg();
