fe7325dc8a57fdd81cc194b2a9f54d7750cf28bc
wangerpangci <18037235202@163.com>
2018-11-17 15:33:35 +0800

    share

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -8,7 +8,7 @@ use app\common\library\Token;
 class Index extends Frontend
 {
 
-    protected $noNeedLogin = [];
+    protected $noNeedLogin = ['*'];
     protected $noNeedRight = ['*'];
     protected $layout = '';
 
@@ -19,8 +19,8 @@ class Index extends Frontend
 
     public function index()
     {
-//        $token=$this->request->get('token');
-        $token=$this->auth->getToken();
+        $token=$this->request->get('token');
+//        $token=$this->auth->getToken();
         $this->assign('token',$token);
 
         return $this->view->fetch();
