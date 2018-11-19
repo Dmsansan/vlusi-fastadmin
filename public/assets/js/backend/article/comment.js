5fe63748dec5705d494ff1c01a73e7c172b61276
wangerpangci <18037235202@163.com>
2018-11-19 11:59:50 +0800

    edit

--- public/assets/js/backend/article/comment.js
+++ public/assets/js/backend/article/comment.js
@@ -6,8 +6,8 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
             Table.api.init({
                 extend: {
                     index_url: 'article/comment/index',
-                    add_url: 'article/comment/add',
-                    edit_url: 'article/comment/edit',
+                    // add_url: 'article/comment/add',
+                    // edit_url: 'article/comment/edit',
                     del_url: 'article/comment/del',
                     multi_url: 'article/comment/multi',
                     table: 'article_comment',
@@ -29,9 +29,10 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                         {field: 'content', title: __('Content')},
                         {field: 'img', title: __('Img'),formatter: Controller.api.formatter.url, operate: false},
                         {field: 'user.nickname', title: __('评论人')},
-                        {field: 'user.avatar',title: __('用户头像'),formatter: Table.api.formatter.image, operate: false},
+                        {field: 'user.avatar',title: __('用户头像'),formatter: Controller.api.formatter.myimage, operate: false},
                         {field: 'createtime', title: __('Createtime'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
-                        {field: 'createtime', title: __('Createtime'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+
                     ]
                 ]
             });
@@ -56,8 +57,18 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                 url: function (value, row, index) {
                     return '<a href="' +row.img + '" target="_blank" class="label bg-green"></a>';
                 },
+                myimage: function (value, row, index) {
+                    var classname = typeof this.classname !== 'undefined' ? this.classname : 'img-sm img-center';
+                    if(value){
+                        return '<a href="' + Fast.api.cdnurl(value) + '" target="_blank"><img class="' + classname + '" src="' + Fast.api.cdnurl(value) + '" /></a>';
+                    }else{
+                        return '-';
+
+                    }
+                },
 
             }
+
         }
     };
     return Controller;
--- public/assets/js/backend/course/comment.js
+++ public/assets/js/backend/course/comment.js
@@ -29,7 +29,7 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                         {field: 'img', title: __('Img'),formatter: Controller.api.formatter.url, operate: false},
                         {field: 'content', title: __('评论内容'), operate: false},
                         {field: 'user.nickname', title: __('评论人')},
-                        {field: 'user.avatar',title: __('用户头像'),formatter: Table.api.formatter.image, operate: false},
+                        {field: 'user.avatar',title: __('用户头像'),formatter: Controller.api.formatter.myimage, operate: false},
                         {field: 'createtime', title: __('Createtime'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
                         {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
                     ]
@@ -54,11 +54,19 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
             formatter: {
 
                 url: function (value, row, index) {
-                    console.log(vale);
                     if(value){
                         return '<a href="' +row.img + '" target="_blank" class="label bg-green"></a>';
                     }else{
                         return '-';
+                    }
+                },
+                myimage: function (value, row, index) {
+                    var classname = typeof this.classname !== 'undefined' ? this.classname : 'img-sm img-center';
+                    if(value){
+                        return '<a href="' + Fast.api.cdnurl(value) + '" target="_blank"><img class="' + classname + '" src="' + Fast.api.cdnurl(value) + '" /></a>';
+                    }else{
+                        return '-';
+
                     }
                 },
 
