548969405a5d80ad4b0ebd186fa5dbd635cffe40
wangerpangci <18037235202@163.com>
2018-11-20 00:04:12 +0800

    online 0004

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -24,8 +24,6 @@ class Index extends Frontend
     public function index()
     {
 //        $token=$this->request->get('token');
-
-
         $token=$this->auth->getToken();
         $this->assign('token',$token);
 
